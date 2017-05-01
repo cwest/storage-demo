a8677b6492b810ab81fb6ec2c36e9cf6ee7168e1
Casey West <casey@geeknest.com>
2017-05-01 08:40:14 -0400

    Fixing up the fresh start.

--- fresh-start.sh
+++ fresh-start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
-gsutil rm gs://$(cf env casey-storage-sample | ag bucket_name | perl -p -e's/^/{/;s/$/}/' | jq -r '.bucket_name')/*
+gsutil -m rm gs://$(cf env casey-storage-sample | ag bucket_name | perl -p -e's/^/{/;s/$/}/' | jq -r '.bucket_name')/*
 
 cf delete -f casey-storage-sample
 cf delete-service -f storage-service
 cf delete-orphaned-routes -f
 
-fly -t lite destroy-pipeline -p storage-demo
-fly -t lite set-pipeline -p storage-demo -c ci/pipeline.yml -l ~/.gcp-config.yml
-fly -t lite unpause-pipeline -p storage-demo
+fly -t concourse destroy-pipeline -p storage-sample
+fly -t concourse set-pipeline -p storage-sample -c ci/pipeline.yml -l ~/.gcp-config.yml
+fly -t concourse unpause-pipeline -p storage-sample
